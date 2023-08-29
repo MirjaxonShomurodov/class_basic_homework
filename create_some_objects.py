7f36d5539f97cf1022f9ecf5be4b8b924bfec92a
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-08-29 06:08:50 +0500

    objects

--- create_some_objects.py
+++ create_some_objects.py
@@ -2,3 +2,14 @@ from create_one_attribute import Person
 
 #create an object named "p1" whose name is "Anvar"
 #create an object named "p2" whose name is "Shavkat"
+
+class Person:
+    def __init__(self,name,nam) -> None:
+        self.name = name
+        self.nam = nam
+
+P1 = Person("Anvar")
+P2 = Person("Shavkat")
+
+print(P1.name)
+print(P2.nam)
